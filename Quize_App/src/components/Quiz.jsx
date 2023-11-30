9725f68da1ed359ffa3ed8ed9da2273035ee2ba3
Arina Siedina <orynasiedina@gmail.com>
2023-11-30 13:51:15 +0100

    Quiz component

--- Quize_App/src/components/Quiz.jsx
+++ Quize_App/src/components/Quiz.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import QUESTIONS from "../questions";
+import { useState } from "react";
 
 const Quiz = () => {
-  return (
-    <div>Quiz</div>
-  )
-}
+  const [userAnswer, setUserAnswer] = useState([]);
+  const activeQuestionIndex = userAnswer.length;
 
-export default Quiz
+  function handleSelectAnswer(answer){
+      setUserAnswer((prevUserAnswers)=>{
+        return[...prevUserAnswers, answer]
+      })
+  }
+  return (
+    <div id="question">
+      <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+      <ul id="answers">
+        {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+          <li key={answer} className="answer">
+            <button onClick={(e)=>handleSelectAnswer(answer)}>{answer}</button>
+            </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Quiz;
