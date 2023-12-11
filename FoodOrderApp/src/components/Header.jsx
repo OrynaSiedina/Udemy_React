ef10fae41657e9c8be1bdcbb6c444921fa2a1d0e
OrynaSiedina <146128335+OrynaSiedina@users.noreply.github.com>
2023-12-11 18:34:31 +0100

    Update Header.jsx

--- FoodOrderApp/src/components/Header.jsx
+++ FoodOrderApp/src/components/Header.jsx
@@ -7,11 +7,7 @@ const Header = () => {
         <img src={logo} alt="logo" />
         <h1>React food</h1>
       </div>
-<<<<<<< HEAD
       <nav><button>Cart (0)</button></nav>
-=======
-      <nav><button>Cart</button></nav>
->>>>>>> cee4e7039ee1c1e6c28177352cc4c550fd4adcde
     </header>
   );
 };
