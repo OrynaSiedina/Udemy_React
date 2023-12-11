173f84a4e2534a71bc1b34d0f826c2809db065bb
Arina Siedina <orynasiedina@gmail.com>
2023-12-11 19:06:53 +0100

    price price formater added

--- FoodOrderApp/src/components/Card.jsx
+++ FoodOrderApp/src/components/Card.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { priceFormater } from '../utils/priceFormater'
 
 const Card = ({meal}) => {
   return (
@@ -7,7 +8,7 @@ const Card = ({meal}) => {
         <img src={`http://localhost:3000/${meal.image}`} alt={meal.name} />
       <div>
         <h3>{meal.name}</h3>
-        <p className='meal-item-price'>{meal.price}</p>
+        <p className='meal-item-price'>{priceFormater.format(meal.price)}</p>
         <p className='meal-item-description'>{meal.description}</p>
       </div>
         <button className='meal-item-actions'>Add to Cart</button>
---
+++ FoodOrderApp/src/utils/priceFormater.js
@@ -0,0 +1,4 @@
+export const priceFormater = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+})
